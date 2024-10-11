22274e872e144281a8a4a8071e292d846c21d921
Matteo <matteo.ciardi26@gmail.com>
2024-10-11 09:52:46 +0200

    es in corso

---
+++ Es_21_Array_Associativo_e_Voti.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array Associativo</title>
+</head>
+<body style="background-color: lightblue;">
+
+<?php 
+    $studente=array("cognome" => "Gori" , "nome" => "Mattia" ,"listaVoti" => array(random_int(2,10),random_int(2,10),random_int(2,10),random_int(2,10),random_int(2,10)));
+    
+    $str="<ul>";
+    foreach ($studente as $chiave => $valore) {
+        if($chiave == "listaVoti"){
+            $str = $str . "<ol>";
+            for ($i=0; $i < count($studente["listaVoti"]); $i++) { 
+                $str = $str . "<li>".$studente["listaVoti"][$i]."</li>"; 
+            }
+            $str = $str . "</ol>";
+            
+        } else {
+            $str = $str . "<li>".$valore."</li>";
+        }
+        $str = $str . "</ul>";
+
+    }
+    echo "$str";
+   
+  
+?>
+
+
+</table>
+</body>
+</html>
